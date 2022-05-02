e0badeb2b0b06a5cb264d2dd3c75f9a2bfdae975
Omar Brikaa <brikaaomar@gmail.com>
2022-05-02 14:35:50 +0200

    Added websockets

--- test.pl
+++ test.pl
@@ -1,11 +1,3 @@
-# Asking user for Input
-print "What is your age?\n";
-
-# Getting an age from the user
-$age = <STDIN>;
-
-# Removes new line from the input
-chomp $age;
-
-# Printing the value entered by user
-print "Your age is ", $age;
+print "What is your name?\n";
+$name = <>;
+print "Your name is ",$name;
